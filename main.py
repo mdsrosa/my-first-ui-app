3a9a52dad9b1590a4bbf113f5c231de49be0e3cb
Matheus Rosa <matheusdsrosa@gmail.com>
2014-11-12 19:34:23 -0200

    Adding an Icon

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import sys
 from PyQt4 import QtGui
 
 
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.init()
+
+    def init(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+
+        self.show()
+
+
 def main():
     app = QtGui.QApplication(sys.argv)
-
-    window = QtGui.QWidget()
-    window.resize(250, 150)
-    window.move(300, 300)
-    window.setWindowTitle('Simple Window')
-    window.show()
-
-
+    ex = Example()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
